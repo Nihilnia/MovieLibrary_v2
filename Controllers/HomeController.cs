7b4b5685007c70d3d4ef07e1f369c25a4f16c59c
Nihil <35506793+Nihilnia@users.noreply.github.com>
2022-01-02 03:21:18 +0300

    asdasd

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -14,7 +14,6 @@ namespace Faint.Controllers
         {
             //DatabaseProcesses.AddRandomUsers(50);
             //DatabaseProcesses.AddRandomMovies(200);
-            //asdasd
             return View();
         }
 
